298ecf970d4a474bd308c4b4bf5c77302a08b39b
Neeraj Kumar Singh <Neeraj.K.Singh1@aexp.com>
2019-02-24 21:29:46 -0700

    Initial Commit

---
+++ moviemicroservice/src/main/java/com/neeraj/microservice/movies/movieservice/MovieserviceApplication.java
@@ -0,0 +1,13 @@
+package com.neeraj.microservice.movies.movieservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieserviceApplication.class, args);
+    }
+
+}
