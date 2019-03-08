5ad288b55ea63b54811f18f30fcb432f8ea0a9cb
Neeraj Kumar Singh <Neeraj.K.Singh1@aexp.com>
2019-03-07 18:35:57 -0700

    Added a few query function for the name basic and title basic service.

--- MoviesGraphQLService/src/main/java/com/neeraj/microservice/movies/movieservice/graphql/resolver/QueryResolver.java
+++ MoviesGraphQLService/src/main/java/com/neeraj/microservice/movies/movieservice/graphql/resolver/QueryResolver.java
@@ -1,13 +1,17 @@
 package com.neeraj.microservice.movies.movieservice.graphql.resolver;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.neeraj.microservice.movies.movieservice.graphql.model.domain.NameBasicsTitleBasicsDto;
+import com.neeraj.microservice.movies.movieservice.graphql.model.domain.TitleBasicsNamePrincipleDto;
 import com.neeraj.microservice.movies.movieservice.graphql.model.entity.NameBasics;
+import com.neeraj.microservice.movies.movieservice.graphql.model.entity.TitleBasics;
 import com.neeraj.microservice.movies.movieservice.graphql.service.NameBasicsService;
 import com.neeraj.microservice.movies.movieservice.graphql.service.TitleBasicsService;
 import com.neeraj.microservice.movies.movieservice.graphql.service.TitleCrewService;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class QueryResolver implements GraphQLQueryResolver {
@@ -22,7 +26,35 @@ public class QueryResolver implements GraphQLQueryResolver {
         this.titleCrewService = titleCrewService;
     }
 
+    public Optional<NameBasics> getArtist(String artistId) {
+        return nameBasicsService.searchArtistById(artistId);
+    }
+
+    public NameBasicsTitleBasicsDto getArtistandtitles(String artistId) {
+        return nameBasicsService.searchByArtistIdWithTitles(artistId);
+    }
+
     public List<NameBasics> getArtists(String name, Integer maxResults, Integer pageNumber) {
         return nameBasicsService.searchArtistByName(name, maxResults, pageNumber);
+    }
+
+    public List<NameBasicsTitleBasicsDto> getArtistsandtitles(String name, Integer maxResults, Integer pageNumber) {
+        return nameBasicsService.searchArtistByNameWithTitles(name, maxResults, pageNumber);
+    }
+
+    public TitleBasics getTitle(String titleID) {
+        return titleBasicsService.findById(titleID).get();
+    }
+
+    public List<TitleBasics> getTitles(String name, Integer maxResults, Integer pageNumber) {
+        return titleBasicsService.searchTitleBasicsByName(name, maxResults, pageNumber).getContent();
+    }
+
+    public TitleBasicsNamePrincipleDto getTitleandartistsandprinciples(String titleID) {
+        return titleBasicsService.searchTitleBasicByIdWithNameWithPrinciples(titleID);
+    }
+
+    public List<TitleBasicsNamePrincipleDto> getTitlesandartistsandprinciples(String name, Integer maxResults, Integer pageNumber) {
+        return titleBasicsService.searchTitleBasicsByIdWithNameWithPrinciples(name, maxResults, pageNumber);
     }
 }
--- MoviesGraphQLService/src/main/java/com/neeraj/microservice/movies/movieservice/graphql/service/TitleBasicsService.java
+++ MoviesGraphQLService/src/main/java/com/neeraj/microservice/movies/movieservice/graphql/service/TitleBasicsService.java
@@ -2,6 +2,7 @@ package com.neeraj.microservice.movies.movieservice.graphql.service;
 
 import com.neeraj.microservice.movies.movieservice.graphql.exceptions.NoSuchObjectFoundException;
 import com.neeraj.microservice.movies.movieservice.graphql.model.domain.TitleBasicsNameDto;
+import com.neeraj.microservice.movies.movieservice.graphql.model.domain.TitleBasicsNamePrincipleDto;
 import com.neeraj.microservice.movies.movieservice.graphql.model.domain.TitleBasicsPrinciplesDto;
 import com.neeraj.microservice.movies.movieservice.graphql.model.entity.NameBasics;
 import com.neeraj.microservice.movies.movieservice.graphql.model.entity.TitleBasics;
@@ -21,17 +22,21 @@ import java.util.stream.Collectors;
 @Service
 public class TitleBasicsService {
 
-    @Autowired
+
     private TitleBasicsRepository titleBasicsRepository;
-
-    @Autowired
     private TitlePrincipalsRepository titlePrincipalsRepository;
-
-    @Autowired
     private NameBasicsService nameBasicsService;
+    private PagingService pagingService;
+    private ModelMapper modelMapper;
 
     @Autowired
-    private PagingService pagingService;
+    public TitleBasicsService(TitleBasicsRepository titleBasicsRepository, TitlePrincipalsRepository titlePrincipalsRepository, NameBasicsService nameBasicsService, PagingService pagingService) {
+        this.titleBasicsRepository = titleBasicsRepository;
+        this.titlePrincipalsRepository = titlePrincipalsRepository;
+        this.nameBasicsService = nameBasicsService;
+        this.pagingService = pagingService;
+        modelMapper = new ModelMapper();
+    }
 
     public Optional<TitleBasics> findById(String tconst) {
         return titleBasicsRepository.findById(tconst);
@@ -52,29 +57,68 @@ public class TitleBasicsService {
                 .orElseThrow(() -> new NoSuchObjectFoundException("No any Title Found by ID:" + uniqueId));
         List<TitlePrincipals> titlePrincipals = getListOfCorrespondingPrinciples(titleBasics);
 
-        ModelMapper modelMapper = new ModelMapper();
         TitleBasicsPrinciplesDto titleBasicsPrinciplesDto = modelMapper.map(titleBasics, TitleBasicsPrinciplesDto.class);
-        titleBasicsPrinciplesDto.setPrincipals(titlePrincipals);
-        return titleBasicsPrinciplesDto;
+
+        return titleBasicsPrinciplesDto
+                .toBuilder()
+                .principals(titlePrincipals)
+                .build();
     }
 
     private List<TitlePrincipals> getListOfCorrespondingPrinciples(TitleBasics titleBasics) {
         return titlePrincipalsRepository.findByTitlePrincipalsUniqueConstrainTconst(titleBasics.getTconst());
     }
 
+    private List<NameBasics> getListOfCorrespondingNameBasics(List<TitlePrincipals> principals) {
+        return principals
+                .parallelStream()
+                .map(p -> nameBasicsService.searchArtistById(p.getNconst()))
+                .filter(optional -> optional.isPresent())
+//        .filter(
+//            optional -> {
+//              System.out.println(optional);
+//              return optional.isPresent();
+//            })
+                .map(nameBasics -> nameBasics.get())
+                .collect(Collectors.toList());
+    }
+
     public TitleBasicsNameDto searchTitleBasicByIdWithName(String uniqueId) {
         TitleBasics titleBasics = searchTitleBasicsByID(uniqueId)
                 .orElseThrow(() -> new NoSuchObjectFoundException("No any Title Found by ID:" + uniqueId));
 
-        List<NameBasics> nameBasics = getListOfCorrespondingPrinciples(titleBasics)
-                .parallelStream()
-                .map(p -> nameBasicsService.searchArtistById(p.getNconst()).get())
-                .collect(Collectors.toList());
+        List<TitlePrincipals> principles = getListOfCorrespondingPrinciples(titleBasics);
+        List<NameBasics> nameBasics = getListOfCorrespondingNameBasics(principles);
 
-        ModelMapper modelMapper = new ModelMapper();
         TitleBasicsNameDto titleBasicsPrinciplesDto = modelMapper.map(titleBasics, TitleBasicsNameDto.class);
-        titleBasicsPrinciplesDto.setNameBasics(nameBasics);
 
-        return titleBasicsPrinciplesDto;
+        return titleBasicsPrinciplesDto.toBuilder()
+                .nameBasics(nameBasics)
+                .build();
+    }
+
+    public TitleBasicsNamePrincipleDto searchTitleBasicByIdWithNameWithPrinciples(String uniqueId) {
+
+        TitleBasics titleBasics = searchTitleBasicsByID(uniqueId)
+                .orElseThrow(() -> new NoSuchObjectFoundException("No any Title Found by ID:" + uniqueId));
+        List<TitlePrincipals> principles = getListOfCorrespondingPrinciples(titleBasics);
+        List<NameBasics> nameBasics = getListOfCorrespondingNameBasics(principles);
+
+        TitleBasicsNamePrincipleDto titleBasicsNamePrincipleDto = modelMapper.map(titleBasics, TitleBasicsNamePrincipleDto.class);
+
+        return titleBasicsNamePrincipleDto
+                .toBuilder()
+                .principals(principles)
+                .artists(nameBasics)
+                .build();
+    }
+
+    public List<TitleBasicsNamePrincipleDto> searchTitleBasicsByIdWithNameWithPrinciples(String searchString, Integer maxResults, Integer pageNumber) {
+
+        return searchTitleBasicsByName(searchString, maxResults, pageNumber)
+                .get()
+                .parallel()
+                .map(titleBasics -> searchTitleBasicByIdWithNameWithPrinciples(titleBasics.getTconst()))
+                .collect(Collectors.toList());
     }
 }
